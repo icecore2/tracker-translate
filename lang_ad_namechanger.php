b22bcef7475142a167179f162bdf7990f2fc581d
icecore2 <dj.icecore@gmail.com>
2017-09-24 00:53:51 +0300

    Add files via upload

---
+++ lang_ad_namechanger.php
@@ -0,0 +1,20 @@
+<?php
+$lang = array(
+'namechanger_err' => "Error",
+'namechanger_missing' => "Username or ID missing",
+'namechanger_cannot' => "Ntz, Ntz... we cannnot let you change staff names.",
+'namechanger_changed_to' => "Your Username Has Been Changed To",
+'namechanger_changed' => "Username changed",
+'namechanger_borked' => "Borked",
+'namechanger_already_exist' => "Username already exists!",
+'namechanger_success' => "Success",
+'namechanger_u_changed' => "Username Has Been Changed To ",
+'namechanger_please' => " please wait while you are redirected",
+'namechanger_change_u' => "Change Username",
+'namechanger_id' => "Old Username Account ID ",
+'namechanger_new_user' => "New Username Account name ",
+'namechanger_if' => "If You Are Sure Then ",
+'namechanger_change_name' => "Change Name!",
+'namechanger_stdhead' => "Username Changer"
+);
+?>
