e5b07faf8fa4f19f42cf8e2766d1fc8de2199852
icecore2 <dj.icecore@gmail.com>
2017-09-24 14:02:05 +0300

    edit translation

--- lang_ban_clients.php
+++ lang_ban_clients.php
@@ -1,19 +1,19 @@
 <?php
 $lang = array (
 #ban clients
-'ban_clients_stderr' => 'Error',
-'ban_clients_msg1' => 'This client is already banned',
-'ban_clients_stderr1' => 'Success',
-'ban_clients_msg2' => 'This client has been added to the banned list',
-'ban_clients_stdhead' => 'Ban Clients',
-'ban_clients_msg3' => 'You must enter a reason!',
+'ban_clients_stderr' => 'שגיאה',
+'ban_clients_msg1' => 'תוכנה זו כבר מושעית',
+'ban_clients_stderr1' => 'הצלחה',
+'ban_clients_msg2' => 'תוכנה זו נוספה אל רשימת ההשעיה',
+'ban_clients_stdhead' => 'השעיית תוכנה',
+'ban_clients_msg3' => 'חובה לרשום סיבת השעייה!',
 'ban_clients_msg4' => 'By visiting this page you are indicating that you want to ban the following client:',
-'ban_clients_client' => 'Client',
+'ban_clients_client' => 'תוכנה',
 'ban_clients_uagent' => 'User Agent',
 'ban_clients_peerid' => 'Peer Id',
 'ban_clients_peeridasci' => 'Peer Id Ascii',
-'ban_clients_reason' => 'Reason',
-'ban_clients_banall' => 'Ban all versions?',
-'ban_clients_stdhead2' => 'Ban BitTorrent Clients',
+'ban_clients_reason' => 'סיבה',
+'ban_clients_banall' => 'השעיית כל הגרסאות?',
+'ban_clients_stdhead2' => 'השעיית BitTorrent תוכנות',
 )
 ?>
